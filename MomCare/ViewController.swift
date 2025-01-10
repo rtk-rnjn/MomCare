4b2a3c6fe2315877f2b15c7aba1d4538fa00e3fc
Ritik Ranjan <ritik0ranjan+github@gmail.com>
2025-01-10 09:23:10 +0530

    Initial Commit

---
+++ MomCare/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MomCare
+//
+//  Created by Ritik Ranjan on 10/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
