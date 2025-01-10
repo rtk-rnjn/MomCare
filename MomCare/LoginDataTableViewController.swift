74f8e9b69cf05245cf81629f524017884dccf6f5
KHUSHI <123815743+feliz2023@users.noreply.github.com>
2025-01-10 16:59:28 +0530

    login first commit

---
+++ MomCare/LoginDataTableViewController.swift
@@ -0,0 +1,26 @@
+//
+//  LoginDataTableViewController.swift
+//  MomCare
+//
+//  Created by Batch - 2  on 10/01/25.
+//
+
+import UIKit
+
+class LoginDataTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("hi")
+
+        // Uncomment the following line to preserve selection between presentations
+//         self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    
+    
+
+    
+}
