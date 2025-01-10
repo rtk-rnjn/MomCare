168d70ba761d756d09e3815e7ce27f4eec94868b
Ritik Ranjan <ritik0ranjan+github@gmail.com>
2025-01-10 16:56:25 +0530

    Add image, buttons, and text to FrontPage
    TODO: Fix this

--- MomCare/Controllers/ViewController.swift
+++ MomCare/Controllers/ViewController.swift
@@ -7,13 +7,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    @IBOutlet var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        collectionView.backgroundColor = .none
     }
 
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return FrontPageData.images.count
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "slide", for: indexPath) as! FrontPageSliderCollectionViewCell
+
+        cell.imageView.image = FrontPageData.images[indexPath.row].image
+        cell.heading.text = FrontPageData.images[indexPath.row].heading
+
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
+    }
 
 }
 
