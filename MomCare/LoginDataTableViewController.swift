44dc8d77ab7f47a4cce9d4158a64f17d7b5636b9
KHUSHI <123815743+feliz2023@users.noreply.github.com>
2025-01-13 14:22:01 +0530

    Login Page added

--- MomCare/LoginDataTableViewController.swift
+++ MomCare/LoginDataTableViewController.swift
@@ -11,13 +11,6 @@ class LoginDataTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("hi")
-
-        // Uncomment the following line to preserve selection between presentations
-//         self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
     
     
