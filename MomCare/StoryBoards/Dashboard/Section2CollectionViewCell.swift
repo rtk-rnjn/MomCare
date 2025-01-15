7f1bdc21a89fe9395bec66c671fa7d8d6ae4bd2a
Nupur Sharma <write2nupu@gmail.com>
2025-01-15 16:43:49 +0530

    changes

---
+++ MomCare/StoryBoards/Dashboard/Section2CollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  Section2CollectionViewCell.swift
+//  MomCare
+//
+//  Created by Batch-2 on 15/01/25.
+//
+
+import UIKit
+
+class Section2CollectionViewCell: UICollectionViewCell {
+    
+}
