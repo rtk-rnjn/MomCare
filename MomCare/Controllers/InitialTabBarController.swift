2fca82fb7a49f39b0d5d3e909e9bce2a21190be0
Ritik Ranjan <ritik0ranjan+github@gmail.com>
2025-01-16 16:36:19 +0530

    Remove "Forced" Navbar from TabBar

---
+++ MomCare/Controllers/InitialTabBarController.swift
@@ -0,0 +1,22 @@
+//
+//  InitialViewController.swift
+//  MomCare
+//
+//  Created by Ritik Ranjan on 16/01/25.
+//
+
+import UIKit
+
+class InitialTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+        navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+
+}
