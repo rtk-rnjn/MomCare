c59e0e388eb24e39a524bda37de548b284fdfbe2
Aryansingh0810 <ss0854850@gmail.com>
2025-01-16 16:22:41 +0530

    commit temp changes

---
+++ MomCare/StoryBoards/MyPlan/ProgressRing.swift
@@ -0,0 +1,38 @@
+//
+//  ProgressRing.swift
+//  MomCare
+//
+//  Created by Batch - 2  on 16/01/25.
+//
+
+import UIKit
+
+
+    class ProgressView: UIView {
+        private var progressRing = UIProgressView(progressViewStyle: .default)
+
+        override init(frame: CGRect) {
+            super.init(frame: frame)
+            setupProgressRing()
+        }
+
+        required init?(coder: NSCoder) {
+            super.init(coder: coder)
+            setupProgressRing()
+        }
+
+        func setupProgressRing() {
+            progressRing.frame = CGRect(x: 0, y: 0, width: 200, height: 20) // Customize size
+            progressRing.progress = 0.5  // Example progress
+            progressRing.trackTintColor = .lightGray
+            progressRing.tintColor = .blue
+            self.addSubview(progressRing)
+        }
+
+        func updateProgress(to value: Float) {
+            progressRing.setProgress(value, animated: true)
+        }
+    }
+
+
+
