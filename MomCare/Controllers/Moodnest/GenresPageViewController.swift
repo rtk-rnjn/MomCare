2e292d4924c18e8d3c64437a25fe7e13b36dcf64
KHUSHI <123815743+feliz2023@users.noreply.github.com>
2025-01-17 17:02:33 +0530

    Changes made on 17 Jan at 5pm

--- MomCare/Controllers/Moodnest/GenresPageViewController.swift
+++ MomCare/Controllers/Moodnest/GenresPageViewController.swift
@@ -7,18 +7,47 @@
 
 import UIKit
 
-class GenresPageViewController: UIViewController {
+class GenresPageViewController: UIViewController, UICollectionViewDataSource {
+   
+    
+    @IBOutlet weak var MoodnestCollectionView: UICollectionView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let navigationBar = UINavigationBar()
+        navigationBar.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(navigationBar)
+        
         let image = UIImage(systemName: "Happy") // Use your desired image here
         let barButtonItem = UIBarButtonItem(image: image, style: .plain, target: self, action: #selector(rightBarButtonTapped))
         
         self.navigationItem.rightBarButtonItem = barButtonItem
+        navigationBar.items = [navigationItem]
+        
+        MoodnestCollectionView.register(UINib(nibName: "MainImage", bundle: nil), forCellWithReuseIdentifier: "MainImage")
+        MoodnestCollectionView.register(UINib(nibName: "MoodNestMultipleImages", bundle: nil), forCellWithReuseIdentifier: "MoodNestMultipleImages")
+        
+        MoodnestCollectionView.delegate = self
+        MoodnestCollectionView.dataSource = self
+        MoodnestCollectionView.collectionViewLayout = MoodnestLayout()
+        
+        
     }
     
     @objc func rightBarButtonTapped() {
            // Action when the button is tapped
            print("Right navigation bar button tapped!")
        }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
 }
+
