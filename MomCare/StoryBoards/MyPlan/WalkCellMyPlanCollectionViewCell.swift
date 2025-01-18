fa71ba6e80f6cfce604d385132c1a95a1d4be10d
Nupur Sharma <write2nupu@gmail.com>
2025-01-19 00:38:27 +0530

    ...

---
+++ MomCare/StoryBoards/MyPlan/WalkCellMyPlanCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  WalkCellMyPlanCollectionViewCell.swift
+//  MomCare
+//
+//  Created by Nupur on 19/01/25.
+//
+
+import UIKit
+
+class WalkCellMyPlanCollectionViewCell: UICollectionViewCell {
+    
+}
