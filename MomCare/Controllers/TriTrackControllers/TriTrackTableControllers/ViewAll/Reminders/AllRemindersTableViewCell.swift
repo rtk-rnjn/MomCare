efc798cd45fdd329fa3cc2323ed76a1b7486f419
Ritik Ranjan <ritik0ranjan+github@gmail.com>
2025-01-20 13:49:34 +0530

    Add chevron from required buttons in TriTrack

---
+++ MomCare/Controllers/TriTrackControllers/TriTrackTableControllers/ViewAll/Reminders/AllRemindersTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  AllRemindersTableViewCell.swift
+//  MomCare
+//
+//  Created by Ritik Ranjan on 20/01/25.
+//
+
+import UIKit
+
+class AllRemindersTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ MomCare/Controllers/TriTrackControllers/TriTrackTableControllers/ViewAll/Symptoms/AllSymptomsTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  AllSymptomsTableViewCell.swift
+//  MomCare
+//
+//  Created by Ritik Ranjan on 20/01/25.
+//
+
+import UIKit
+
+class AllSymptomsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
