bfe977209e6acd5e8dba755a14c264c5f89a8687
Nupur Sharma <write2nupu@gmail.com>
2025-01-22 11:02:33 +0530

    button added

--- MomCare/Controllers/MyPlan/Diet/HeaderTableViewCell.swift
+++ MomCare/Controllers/MyPlan/Diet/HeaderTableViewCell.swift
@@ -1,12 +1,27 @@
-//
-//  HeaderTableViewCell.swift
-//  MomCare
-//
-//  Created by Batch - 2  on 20/01/25.
-//
-
 import UIKit
 
 class HeaderTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var actionButton: UIButton!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        configurePullDownMenu()
+    }
+
+    private func configurePullDownMenu() {
+        // Create menu items (SF Symbols)
+        let addItem = UIAction(title: "Add Item", image: UIImage(systemName: "plus")) { _ in
+            print("Add Item selected")
+        }
+        let replaceItem = UIAction(title: "Replace Item", image: UIImage(systemName: "repeat")) { _ in
+            print("Replace Item selected")
+            
+        }
+        actionButton.menu = UIMenu(title: "", children: [addItem, replaceItem])
+        actionButton.showsMenuAsPrimaryAction = true
+
+        actionButton.setTitle(nil, for: .normal)
+        actionButton.setImage(UIImage(systemName: "square.and.pencil"), for: .normal)
+    }
 }
