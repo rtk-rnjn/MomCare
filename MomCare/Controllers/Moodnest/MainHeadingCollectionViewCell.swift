1fab71b31922f65f8470ca99095be0edb34b61e1
KHUSHI <123815743+feliz2023@users.noreply.github.com>
2025-01-22 13:35:10 +0530

    excercise images added
    ...

--- MomCare/Controllers/Moodnest/MainHeadingCollectionViewCell.swift
+++ MomCare/Controllers/Moodnest/MainHeadingCollectionViewCell.swift
@@ -1,15 +1,3 @@
-//
-
-//  MainHeadingCollectionViewCell.swift
-
-//  MomCare
-
-//
-
-//  Created by Batch - 2  on 18/01/25.
-
-//
-
 import UIKit
 
 class MainHeadingCollectionViewCell: UICollectionViewCell {
@@ -17,9 +5,25 @@ class MainHeadingCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var mainHeadingLabel: UILabel!
 
     func updateSection1(with indexPath: IndexPath) {
+        let text = "Take a moment to rewind with \"\(FeaturedPlaylists.playlists[indexPath.row].name)\" playlists"
 
-        mainHeadingLabel.text = "Take a moment to rewind with \"\(FeaturedPlaylists.playlists[indexPath.row].name)\" playlists"
+        // Get the preferred large title font and make it bold
+        let baseFont = UIFont.preferredFont(forTextStyle: .largeTitle)
+        let boldFontDescriptor = baseFont.fontDescriptor.withSymbolicTraits(.traitBold) // Add bold trait
+        let boldFont = UIFont(descriptor: boldFontDescriptor ?? baseFont.fontDescriptor, size: baseFont.pointSize)
 
+        // Paragraph style for line spacing
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = -10
+
+        // Combine bold font and paragraph style
+        let attributes: [NSAttributedString.Key: Any] = [
+            .font: boldFont,
+            .paragraphStyle: paragraphStyle
+        ]
+
+        // Create attributed string and set it to the label
+        let attributedText = NSAttributedString(string: text, attributes: attributes)
+        mainHeadingLabel.attributedText = attributedText
     }
-
 }
