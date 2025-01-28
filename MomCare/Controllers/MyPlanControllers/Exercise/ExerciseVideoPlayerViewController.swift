cf75d10e1874e235e687559545867eb6faab36e5
KHUSHI <123815743+feliz2023@users.noreply.github.com>
2025-01-28 15:54:32 +0530

    moodnest

---
+++ MomCare/Controllers/MyPlanControllers/Exercise/ExerciseVideoPlayerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ExerciseVideoPlayerViewController.swift
+//  MomCare
+//
+//  Created by Batch - 2  on 28/01/25.
+//
+
+import UIKit
+
+class ExerciseVideoPlayerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
