fe943055aa9e48a9b36f3e0530182909a5678562
KHUSHI <123815743+feliz2023@users.noreply.github.com>
2025-01-29 11:13:28 +0530

    Music player (#109)
    * Add Gradient
    Gradient to the music player.

---
+++ MomCare/Controllers/MoodnestControllers/PlayerViewController.swift
@@ -0,0 +1,68 @@
+//
+//  PlayerViewController.swift
+//  MomCare
+//
+//  Created by Batch - 2  on 29/01/25.
+//
+
+import UIKit
+import CoreImage
+
+class PlayerViewController: UIViewController {
+
+    // MARK: - OUTLETS
+    @IBOutlet var playerImageViewOutlet: UIImageView!
+
+    let gradientLayer = CAGradientLayer()
+
+        override func viewDidLoad() {
+            super.viewDidLoad()
+
+            updateUIForNewSong(songImage: UIImage(named: "fantasize")!)
+        }
+
+    func updateGradientBackground(with color: UIColor) {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = self.view.bounds
+
+        let lighterColor = color.withAlphaComponent(0.2).cgColor
+        let darkerColor = color.withAlphaComponent(0.7).cgColor
+
+        gradientLayer.colors = [lighterColor, darkerColor]
+        gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.0)
+        gradientLayer.endPoint = CGPoint(x: 0.5, y: 1.0)
+
+        self.view.layer.insertSublayer(gradientLayer, at: 0)
+    }
+
+    func updateUIForNewSong(songImage: UIImage) {
+        if let dominantColor = songImage.dominantColor() {
+            updateGradientBackground(with: dominantColor)
+        }
+    }
+}
+
+extension UIImage {
+    func dominantColor() -> UIColor? {
+        guard let inputImage = CIImage(image: self) else { return nil }
+        let extent = inputImage.extent
+        let filter = CIFilter(name: "CIAreaAverage",
+                               parameters: [kCIInputImageKey: inputImage,
+                                            kCIInputExtentKey: CIVector(cgRect: extent)])
+
+        guard let outputImage = filter?.outputImage else { return nil }
+        var bitmap = [UInt8](repeating: 0, count: 4)
+        let context = CIContext()
+        context.render(outputImage,
+                       toBitmap: &bitmap,
+                       rowBytes: 4,
+                       bounds: CGRect(x: 0, y: 0, width: 1, height: 1),
+                       format: .RGBA8,
+                       colorSpace: CGColorSpaceCreateDeviceRGB())
+
+        return UIColor(red: CGFloat(bitmap[0]) / 255.0,
+                       green: CGFloat(bitmap[1]) / 255.0,
+                       blue: CGFloat(bitmap[2]) / 255.0,
+                       alpha: 1.0)
+    }
+}
---
+++ MomCare/Controllers/MyPlanControllers/Exercise/ExerciseVideoPlayerViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ExerciseVideoPlayerViewController.swift
+//  MomCare
+//
+//  Created by Batch - 2  on 28/01/25.
+//
+
+import UIKit
+
+class ExerciseVideoPlayerViewController: UIViewController {
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
