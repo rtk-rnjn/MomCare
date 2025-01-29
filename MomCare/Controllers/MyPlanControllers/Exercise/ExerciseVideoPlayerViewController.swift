37edaed7d4f6560814cacf69aa6370c8324b3102
KHUSHI <123815743+feliz2023@users.noreply.github.com>
2025-01-29 11:10:05 +0530

    ..player gradient

--- MomCare/Controllers/MyPlanControllers/Exercise/ExerciseVideoPlayerViewController.swift
+++ MomCare/Controllers/MyPlanControllers/Exercise/ExerciseVideoPlayerViewController.swift
@@ -9,13 +9,6 @@ import UIKit
 
 class ExerciseVideoPlayerViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-    
-
     /*
     // MARK: - Navigation
 
