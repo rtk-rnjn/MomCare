abb99969515aa7a17895fdd6b1efd091a4be0cda
Nupur Sharma <write2nupu@gmail.com>
2025-02-04 12:07:25 +0530

    implement scroll view without text

--- MomCare/Controllers/MoodnestControllers/MoodnestViewController.swift
+++ MomCare/Controllers/MoodnestControllers/MoodnestViewController.swift
@@ -7,59 +7,11 @@
 
 import UIKit
 
-class MoodnestViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
-    @IBOutlet var collectionView: UICollectionView!
+class MoodnestViewController: UIViewController {
+    
     @IBOutlet var pageControl: UIPageControl!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        collectionView.dataSource = self
-        collectionView.delegate = self
-        collectionView.showsHorizontalScrollIndicator = false
     }
-
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 1
-    }
-
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return AllMoods.moods.count
-    }
-
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FacesCollectionViewCell", for: indexPath) as? FacesCollectionViewCell
-
-        guard let cell else { fatalError() }
-
-        cell.updateElements(with: AllMoods.moods[indexPath.item])
-        return cell
-    }
-
-    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-        let pageWidth = collectionView.frame.width
-        let currentPage = Int((collectionView.contentOffset.x + pageWidth / 2) / pageWidth)
-        pageControl.currentPage = currentPage
-    }
-}
-
-extension MoodnestViewController: UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: 200, height: 200)
-    }
-
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let selectedImage = AllMoods.moods[indexPath.item].image
-        performSegue(withIdentifier: "ShowGenres", sender: selectedImage)
-    }
-
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "ShowGenres" {
-            if let destination = segue.destination as? GenresPageViewController,
-               let selectedImage = sender as? UIImage {
-                destination.IconImageVar = selectedImage
-            }
-        }
-    }
-
 }
