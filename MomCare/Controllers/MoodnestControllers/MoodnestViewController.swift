d7567a08764925c2ce128b19d00a4d8c58ed077e
Nupur Sharma <write2nupu@gmail.com>
2025-02-04 15:25:53 +0530

    Adding Tap Gesture in the emojis imageView

--- MomCare/Controllers/MoodnestControllers/MoodnestViewController.swift
+++ MomCare/Controllers/MoodnestControllers/MoodnestViewController.swift
@@ -10,8 +10,35 @@ import UIKit
 class MoodnestViewController: UIViewController {
     
     @IBOutlet var pageControl: UIPageControl!
+    @IBOutlet weak var happyImageView: UIImageView!
+    @IBOutlet weak var sadImageView: UIImageView!
+    @IBOutlet weak var stressedImageView: UIImageView!
+    @IBOutlet weak var angryImageView: UIImageView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        happyImageView.isUserInteractionEnabled = true
+        sadImageView.isUserInteractionEnabled = true
+        stressedImageView.isUserInteractionEnabled = true
+        angryImageView.isUserInteractionEnabled = true
+        
+        addTapGesture(to: happyImageView)
+        addTapGesture(to: sadImageView)
+        addTapGesture(to: stressedImageView)
+        addTapGesture(to: angryImageView)
+
+    }
+    
+    func addTapGesture(to imageView: UIImageView) {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(imageTapped(_:)))
+        imageView.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc func imageTapped(_ sender: UITapGestureRecognizer) {
+        if let destinationVC = storyboard?.instantiateViewController(withIdentifier: "genresPageView") as? GenresPageViewController {
+            navigationController?.pushViewController(destinationVC, animated: true)
+        }
     }
 }
