a5cdac3fc27bc81d24da01462a8e303158547600
KHUSHI <123815743+feliz2023@users.noreply.github.com>
2025-02-07 12:33:49 +0530

    Revert "Add Views"
    This reverts commit b5cb102ae25e8e63f4d37e94e4b05e4de8fa46dd.

--- MomCare/Controllers/MyPlanControllers/Exercise/NewExerciseMyPlanCellCollectionViewCell.swift
+++ MomCare/Controllers/MyPlanControllers/Exercise/NewExerciseMyPlanCellCollectionViewCell.swift
@@ -7,10 +7,6 @@
 
 import UIKit
 
-protocol NewExerciseMyPlanCellDelegate: AnyObject {
-    func didTapInfoButton()
-}
-
 class NewExerciseMyPlanCellCollectionViewCell: UICollectionViewCell {
     @IBOutlet var exerciseLevel: UILabel!
     @IBOutlet var exerciseName: UILabel!
@@ -18,19 +14,12 @@ class NewExerciseMyPlanCellCollectionViewCell: UICollectionViewCell {
     @IBOutlet var exerciseImage: UIImageView!
     @IBOutlet var exerciseCompletionPercentage: UILabel!
     @IBOutlet var exerciseStartButton: UIButton!
-    @IBOutlet var infoButton: UIButton!
-    @IBOutlet var dimmedBackgroundView: UIView!
-    
     var completedPercentage: Double = 0
-    var segueHandler: (() -> Void)?
-    weak var delegate: NewExerciseMyPlanCellDelegate?
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        infoButton.addTarget(self, action: #selector(infoButtonTapped), for: .touchUpInside)
-    }
+    var segueHandler: (() -> Void)?
 
     @IBAction func startButtonTapped(_ sender: Any) {
+
         if let segueHandler {
             segueHandler()
         }
@@ -38,9 +27,5 @@ class NewExerciseMyPlanCellCollectionViewCell: UICollectionViewCell {
 
     func updateElements(with handler: (() -> Void)?) {
         segueHandler = handler
-    }
-    
-    @objc func infoButtonTapped(_ sender: UIButton) {
-        delegate?.didTapInfoButton()
     }
 }
