fc50723e9451f196e41089a27530fe825ed1086b
Nupur Sharma <write2nupu@gmail.com>
2025-02-17 14:16:15 +0530

    implement AVKit

--- MomCare/Controllers/MyPlanControllers/Exercise/ExerciseAVPlayerViewController.swift
+++ MomCare/Controllers/MyPlanControllers/Exercise/ExerciseAVPlayerViewController.swift
@@ -16,26 +16,42 @@ class ExerciseAVPlayerViewController: UIViewController {
     @IBOutlet weak var videoView: UIView!
     var player: AVPlayer?
     var playerLayer: AVPlayerLayer?
-
+    var playerViewController: AVPlayerViewController?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Load video from resources
         if let path = Bundle.main.path(forResource: "ExerciseSampleVideo", ofType: "mp4") {
             let videoURL = URL(fileURLWithPath: path)
             setupVideo(url: videoURL)
         }
     }
-
+    
     func setupVideo(url: URL) {
+        // Initialize AVPlayer with the video URL
         player = AVPlayer(url: url)
-        playerLayer = AVPlayerLayer(player: player)
         
-        if let playerLayer = playerLayer {
-            playerLayer.frame = videoView.bounds
-            playerLayer.videoGravity = .resizeAspectFill
-            videoView.layer.addSublayer(playerLayer)
+        // Initialize AVPlayerViewController and set the player
+        playerViewController = AVPlayerViewController()
+        playerViewController?.player = player
+        
+        // Set the player view controller's frame to match the video container
+        if let playerViewController = playerViewController {
+            playerViewController.view.frame = videoView.bounds
+            playerViewController.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
             
-            player?.play()
+            // Add the playerViewController's view as a subview of your custom videoView
+            videoView.addSubview(playerViewController.view)
         }
+    }
+    
+    @IBAction func playButtonTapped(_ sender: UIButton) {
+        player?.play()
+        
+    }
+    
+    @IBAction func pauseButtonTapped(_ sender: UIButton) {
+        player?.pause()
     }
 }
