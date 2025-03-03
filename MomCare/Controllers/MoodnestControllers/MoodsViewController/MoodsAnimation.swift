0aee524ea322bdf19584e1b273d5c8d97b9f161a
Ritik Ranjan <ritik0ranjan+github@gmail.com>
2025-03-03 16:15:45 +0530

    Fix moods

---
+++ MomCare/Controllers/MoodnestControllers/MoodsViewController/MoodsAnimation.swift
@@ -0,0 +1,122 @@
+//
+//  MoodsAnimation.swift
+//  MomCare
+//
+//  Created by Ritik Ranjan on 03/03/25.
+//
+
+import UIKit
+
+extension MoodsViewController {
+    func makeSmile() {
+        let width: CGFloat = smileView.frame.width
+        let height: CGFloat = smileView.frame.height
+ 
+        let smilePath = UIBezierPath()
+        let startX: CGFloat = 0
+        let endX: CGFloat = width
+        let controlX: CGFloat = width / 2
+        let controlY: CGFloat = height * 2
+ 
+        smilePath.move(to: CGPoint(x: startX, y: height / 2))
+        smilePath.addQuadCurve(to: CGPoint(x: endX, y: height / 2), controlPoint: CGPoint(x: controlX, y: controlY))
+ 
+        let smileLayer = CAShapeLayer()
+        smileLayer.path = smilePath.cgPath
+        smileLayer.fillColor = UIColor.clear.cgColor
+        smileLayer.strokeColor = UIColor(hex: "5c4131").cgColor
+        smileLayer.lineWidth = 20
+        smileLayer.lineCap = .round
+ 
+        smileView.layer.addSublayer(smileLayer)
+    }
+
+    func resetTransformations() {
+        leftEyeView.transform = .identity
+        rightEyeView.transform = .identity
+
+        smileView.transform = .identity
+        
+        leftEyeView.animateToFullCircle()
+        rightEyeView.animateToFullCircle()
+    }
+ 
+    func makeHappyFace() {
+        leftEyeView.layer.cornerRadius = leftEyeView.frame.width / 2
+        rightEyeView.layer.cornerRadius = rightEyeView.frame.width / 2
+        
+        view.backgroundColor = UIColor(hex: "#ffd0cb")
+
+        leftEyeView.setColor(hex: "#5c4131")
+        rightEyeView.setColor(hex: "#5c4131")
+        setSmileColor(hex: "#5c4131")
+
+        leftEyeView.transform = CGAffineTransform(scaleX: 1, y: 1)
+        rightEyeView.transform = CGAffineTransform(scaleX: 1, y: 1)
+ 
+        makeSmile()
+    }
+ 
+    func makeSadFace() {
+        leftEyeView.layer.cornerRadius = leftEyeView.frame.width / 2
+        rightEyeView.layer.cornerRadius = rightEyeView.frame.width / 2
+
+        view.backgroundColor = UIColor(hex: "#8DC1D4")
+
+        leftEyeView.setColor(hex: "#2F4C5A")
+        rightEyeView.setColor(hex: "#2F4C5A")
+        setSmileColor(hex: "#2F4C5A")
+ 
+        let scaleTransform = CGAffineTransform(scaleX: 0.8, y: 0.8)
+        leftEyeView.transform = scaleTransform
+        rightEyeView.transform = scaleTransform
+ 
+        let rotateTransform = CGAffineTransform(rotationAngle: CGFloat.pi)
+        smileView.transform = rotateTransform
+    }
+ 
+    func makeStressedFace() {
+        let scaleTransform = CGAffineTransform(scaleX: 0.8, y: 0.2)
+        
+        view.backgroundColor = UIColor(hex: "#D5E5C9")
+
+        leftEyeView.setColor(hex: "#4A593D")
+        rightEyeView.setColor(hex: "#4A593D")
+        setSmileColor(hex: "#4A593D")
+        
+        leftEyeView.transform = scaleTransform
+        rightEyeView.transform = scaleTransform
+ 
+        let rotateTransform = CGAffineTransform(rotationAngle: CGFloat.pi)
+        smileView.transform = rotateTransform
+    }
+
+    func makeAngryFace() {
+        let scaleTransform = CGAffineTransform(scaleX: 0.8, y: 0.8)
+
+        view.backgroundColor = UIColor(hex: "#F79E90")
+        leftEyeView.setColor(hex: "583933")
+        rightEyeView.setColor(hex: "583933")
+        setSmileColor(hex: "583933")
+        
+        let rotateTransform = CGAffineTransform(rotationAngle: CGFloat.pi)
+        
+        let leftTilt = CGAffineTransform(rotationAngle: CGFloat.pi / 6)
+        let rightTilt = CGAffineTransform(rotationAngle: -CGFloat.pi / 6)
+
+        smileView.transform = rotateTransform
+        leftEyeView.animateToSemiCircle()
+        rightEyeView.animateToSemiCircle()
+        
+        leftEyeView.transform = scaleTransform.concatenating(leftTilt)
+        rightEyeView.transform = scaleTransform.concatenating(rightTilt)
+    }
+
+    func setSmileColor(hex: String) {
+        for layer in smileView.layer.sublayers ?? [] {
+            if let layer = layer as? CAShapeLayer {
+                layer.strokeColor = UIColor(hex: hex).cgColor
+            }
+        }
+    }
+}
---
+++ MomCare/Controllers/MoodnestControllers/MoodsViewController/MoodsViewController.swift
@@ -0,0 +1,57 @@
+//
+//  MoodsViewController.swift
+//  MomCare
+//
+//  Created by Batch - 2  on 16/01/25.
+//
+
+import UIKit
+
+class MoodsViewController: UIViewController {
+
+    @IBOutlet var rightEyeView: SemiCircleAnimationView!
+    @IBOutlet var leftEyeView: SemiCircleAnimationView!
+    @IBOutlet var smileView: UIView!
+    @IBOutlet var slider: UISlider!
+    
+    @IBOutlet var moodLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        smileView.backgroundColor = .clear
+        rightEyeView.backgroundColor = .clear
+        leftEyeView.backgroundColor = .clear
+        
+        self.makeHappyFace()
+        self.makeSmile()
+    }
+
+    @IBAction func SetMoodButtonTapped(_ sender: UIButton) {
+        performSegue(withIdentifier: "segueShowMoodNestViewController", sender: nil)
+    }
+
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        slider.value = Float(round(sender.value))
+        
+        UIView.animate(withDuration: 0.5) {
+            self.resetTransformations()
+            switch sender.value {
+            case 0..<1:
+                self.makeHappyFace()
+                self.moodLabel.text = "Happy"
+            case 1..<2:
+                self.makeStressedFace()
+                self.moodLabel.text = "Stressed"
+            case 2..<3:
+                self.makeSadFace()
+                self.moodLabel.text = "Sad"
+            case 3:
+                self.makeAngryFace()
+                self.moodLabel.text = "Angry"
+            default:
+                break
+            }
+        }
+    }
+}
