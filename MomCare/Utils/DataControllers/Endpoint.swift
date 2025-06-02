2e824951af6d2b14b09f2459cd146296298d36e5
Ritik Ranjan <ritik0ranjan+github@gmail.com>
2025-06-03 02:12:33 +0530

    Add APIEndpoints.swift with updated base URL and endpoint definitions

---
+++ MomCare/Utils/DataControllers/Endpoint.swift
@@ -0,0 +1,46 @@
+//
+//  APIEndpoints.swift
+//  MomCare
+//
+//  Created by RITIK RANJAN on 01/06/25.
+//
+
+import Foundation
+
+private let baseURLString = "http://13.233.139.216:8000"
+
+enum Endpoint: String {
+    // Authentication
+    case register = "/auth/register"
+    case login = "/auth/login"
+    case refresh = "/auth/refresh"
+    case update = "/auth/update"
+    case fetch = "/auth/fetch"
+
+    // Meta
+    case meta = "/meta/"
+    case health = "/meta/health"
+    case version = "/meta/version"
+    case ping = "/meta/ping"
+
+    // Content
+    case plan = "/content/plan"
+    case search = "/content/search"
+    case tips = "/content/tips"
+
+    // Content S3
+    case contentS3File = "/content/s3/file/%@"
+    case contentS3Files = "/content/s3/files/%@"
+    case contentS3Directories = "/content/s3/directories/%@"
+    case contentS3Song = "/content/s3/song/%@"
+}
+
+extension Endpoint {
+    var url: String {
+        return baseURLString + self.rawValue
+    }
+
+    func url(with parameters: String...) -> String {
+        return String(format: self.url, arguments: parameters)
+    }
+}
