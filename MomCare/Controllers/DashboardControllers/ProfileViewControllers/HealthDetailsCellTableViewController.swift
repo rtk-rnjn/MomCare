23cdb5e08a8b37c8d24fffa2218955b8cc028210
Ritik Ranjan <ritik0ranjan+github@gmail.com>
2025-06-14 04:27:43 +0530

    Make linter happy
    Co-Authored-By: Aryansingh0810 <147413362+Arayauchiha@users.noreply.github.com>

--- MomCare/Controllers/DashboardControllers/ProfileViewControllers/HealthDetailsCellTableViewController.swift
+++ MomCare/Controllers/DashboardControllers/ProfileViewControllers/HealthDetailsCellTableViewController.swift
@@ -9,13 +9,8 @@ import UIKit
 
 class HealthDetailsCellTableViewController: UITableViewController {
 
-    @IBOutlet weak var HealthDetailLabel: UILabel!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-    }
-    
+    @IBOutlet var HealthDetailLabel: UILabel!
+
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
@@ -23,11 +18,11 @@ class HealthDetailsCellTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return PreExistingCondition.allCases.count
     }
-    
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // Dequeue a reusable cell
         let cell = tableView.dequeueReusableCell(withIdentifier: "HealthDetailCell", for: indexPath)
-        
+
         let preExistingCondition = PreExistingCondition.allCases
         // Set the data
         cell.HealthDetailLabel?.text = preExistingCondition[indexPath.row]
@@ -35,12 +30,10 @@ class HealthDetailsCellTableViewController: UITableViewController {
         return cell
     }
 
-    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if let cell = tableView.cellForRow(at: indexPath) {
             cell.accessoryType = (cell.accessoryType == .checkmark) ? .none : .checkmark
         }
     }
-
 
 }
