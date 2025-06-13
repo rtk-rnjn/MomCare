8dc39d7115a61aff83d5a211955b7cd16ea94feb
Aryansingh0810 <ss0854850@gmail.com>
2025-06-13 17:18:06 +0530

    Update SettingsTableViewController

---
+++ MomCare/Controllers/DashboardControllers/ProfileViewControllers/SettingsTableViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SettingsTableViewController.swift
+//  MomCare
+//
+//  Created by Aryan Singh on 13/06/25.
+//
+
+import UIKit
+
+class SettingsTableViewController: UITableViewController {
+
+    @IBOutlet var userEmail: UILabel!
+    @IBOutlet var userPhoneNumber: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updatePageElements()
+
+    }
+    
+    func updatePageElements(){
+        guard let user = MomCareUser.shared.user else { return }
+        
+        userEmail.text = user.emailAddress
+        userPhoneNumber.text = user.phoneNumber
+    }
+    
+    @IBAction func chnagePassButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+}
