39d599bbd79d4679a8a2a183bdb33921a165f539
Khushi Rana <diaskhushi2025@gmail.com>
2025-06-14 11:53:48 +0530

    Fix Bad code

--- MomCare/Controllers/DashboardControllers/ProfileViewControllers/SettingsTableViewController.swift
+++ MomCare/Controllers/DashboardControllers/ProfileViewControllers/SettingsTableViewController.swift
@@ -9,22 +9,54 @@ import UIKit
 
 class SettingsTableViewController: UITableViewController {
 
-    @IBOutlet var userEmail: UILabel!
-    @IBOutlet var userPhoneNumber: UILabel!
+    @IBOutlet var emailLabel: UILabel!
+    @IBOutlet var phoneNumberLabel: UILabel!
+    @IBOutlet var oldPasswordField: UITableViewCell!
+    @IBOutlet var newPasswordField: UITableViewCell!
+    @IBOutlet var confirmPasswordField: UITableViewCell!
+    @IBOutlet var changePasswordButton: UIButton!
+    
+    var isPasswordFieldsVisible = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         updatePageElements()
-
+        changePasswordButtonArrow()
     }
     
     func updatePageElements(){
         guard let user = MomCareUser.shared.user else { return }
         
-        userEmail.text = user.emailAddress
-        userPhoneNumber.text = user.phoneNumber
+        emailLabel.text = user.emailAddress
+        phoneNumberLabel.text = user.phoneNumber
     }
     
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.section == 1 && indexPath.row == 0{
+            isPasswordFieldsVisible.toggle()
+            changePasswordButtonArrow()
+            
+            tableView.beginUpdates()
+            tableView.endUpdates()
+        }
+    }
     
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.section == 1 && [1, 2, 3].contains(indexPath.row) {
+            return isPasswordFieldsVisible ? UITableView.automaticDimension : 0
+        }
+
+        return UITableView.automaticDimension
+    }
+    
+    func changePasswordButtonArrow() {
+        let imageName = isPasswordFieldsVisible ? "chevron.up" : "chevron.down"
+        let image = UIImage(systemName: imageName)
+        
+        changePasswordButton.contentHorizontalAlignment = .left
+        changePasswordButton.semanticContentAttribute = .forceRightToLeft
+        
+        changePasswordButton.setImage(image, for: .normal)
+        changePasswordButton.configuration?.imagePadding = 160
+    }
 }
--- MomCare/Controllers/TriTrackControllers/TriTrackTableControllers/MainSegmentedView/Symptoms/SymptomsTableViewController.swift
+++ MomCare/Controllers/TriTrackControllers/TriTrackTableControllers/MainSegmentedView/Symptoms/SymptomsTableViewController.swift
@@ -3,7 +3,6 @@
 //  MomCare
 //
 //  Created by Ritik Ranjan on 17/01/25.
-//
 
 import UIKit
 import EventKit
