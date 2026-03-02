bde6471f495d47c856590e05f3f8723dd66abc24
Ritik Ranjan <ritik0ranjan+github@gmail.com>
2026-03-02 17:25:44 +0530

    Update MomCare+/Common/Static/ModelSpecific/DietaryPreferenceData.swift
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- MomCare+/Common/Static/ModelSpecific/DietaryPreferenceData.swift
+++ MomCare+/Common/Static/ModelSpecific/DietaryPreferenceData.swift
@@ -10,7 +10,7 @@ public enum DietaryPreference: String, Codable, CaseIterable, Sendable {
     case nonVegetarian = "Non-Vegetarian"
     case vegan = "Vegan"
 
-    // TODO: Backend does does support more than 3 pref. @rtk-rnjn
+    // TODO: Backend currently supports a maximum of 3 dietary preferences. Add a backend ticket/issue ID here for future follow-up.
     // case pescetarian = "Pescetarian"
     // case flexitarian = "Flexitarian"
     // case glutenFree = "Gluten-Free"
