74e58cf849f91b1277ce0254b0f08e8a806937e3
Aryansingh0810 <ss0854850@gmail.com>
2026-03-14 05:25:50 +0530

    Update DietaryPreferenceData.swift
    Co-Authored-By: Ritik Ranjan <37435729+rtk-rnjn@users.noreply.github.com>

--- MomCare+/Common/Static/ModelSpecific/DietaryPreferenceData.swift
+++ MomCare+/Common/Static/ModelSpecific/DietaryPreferenceData.swift
@@ -2,11 +2,11 @@ public enum DietaryPreference: String, Codable, CaseIterable, Sendable {
     case vegetarian = "veg"
     case nonVegetarian = "non-veg"
     case vegan = "vegan"
-    case pescetarian = "Pescetarian"
-    case flexitarian = "Flexitarian"
-    case glutenFree = "Gluten-Free"
-    case ketogenic = "Ketogenic"
-    case highProtein = "High Protein"
-    case dairyFree = "Dairy-Free"
+//    case pescetarian = "Pescetarian"
+//    case flexitarian = "Flexitarian"
+//    case glutenFree = "Gluten-Free"
+//    case ketogenic = "Ketogenic"
+//    case highProtein = "High Protein"
+//    case dairyFree = "Dairy-Free"
     case none
 }
